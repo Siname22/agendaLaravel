02b301f20805ea9e51bd624263ddc40770388897
mint <rayuga91@gmail.com>
2022-11-24 13:49:21 +0100

    ..

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,6 +1,7 @@
 <div>
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
     <a href='{{ route('categorias.index') }}'>Categorias</a>
+    <br>
     <a href='{{ route('categorias.create') }}'>Crear categorias</a>
 
 </div>
