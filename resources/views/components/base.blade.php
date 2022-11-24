ead5887d70ae35f6551b0bcd68cbf528c7441444
mint <rayuga91@gmail.com>
2022-11-24 13:47:03 +0100

    ..

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -1,11 +1,14 @@
 <html>
-<head>
-    <title>{{ $titulo ?? 'Agenda' }}</title>
-</head>
-<body>
-    <h1>{{ $titulo ?? 'Agenda' }}</h1>
-    <hr/>
-    {{ $slot }}
-    <x-menu/>
-</body>
+    <head>
+        <title>{{ $titulo ?? 'Agenda' }}</title>
+    </head>
+    <body>
+        <h1>{{ $titulo ?? 'Agenda' }}</h1>
+        <hr/>
+        <!--
+        Contenido que hay en categorias_ ...
+        -->
+        {{ $slot }}
+        <x-menu/>
+    </body>
 </html>
