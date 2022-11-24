f0c5ed5b118fbb2ee1fa05585f4c99f30e961ea5
mint <rayuga91@gmail.com>
2022-11-24 14:10:34 +0100

    ..

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,7 +1,8 @@
 <div>
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-    <a href='{{ route('categorias.index') }}'>Categorias</a>
+    <a href='{{ route('categorias.index') }}'>Ver el listado de Categorias</a>
     <br>
     <a href='{{ route('categorias.create') }}'>Crear categorias</a>
+    <br>
 
 </div>
